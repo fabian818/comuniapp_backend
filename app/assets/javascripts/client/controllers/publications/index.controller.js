168ca7d71b332c7d996af7e17e916732e745c191
Fabian Pierre Peña Jacobo <f.pena.jacobo@gmail.com>
2016-06-15 12:50:20 -0500

    Added erase after destroy publication

--- app/assets/javascripts/client/controllers/publications/index.controller.js
+++ app/assets/javascripts/client/controllers/publications/index.controller.js
@@ -4,10 +4,13 @@
 		PublicationService.index(function(data){
 			$scope.publications = data.publications;
 		}); 
-		$scope.destroy = function(publication_id){
-			PublicationService.destroy(publication_id, function(data){
-				$scope.publications = data.publications;
-			}); 
+		$scope.destroy = function(publication){
+			PublicationService.destroy(publication.id, function(data){
+				if (data === true) {
+					var index = $scope.publications.indexOf(publication);
+					$scope.publications.splice(index, 1);
+				}
+			});
 		}
 	}]);
 })();
