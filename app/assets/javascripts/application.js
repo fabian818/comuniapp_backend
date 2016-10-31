a7755d70d5e2feddef6012f5833ac17aaba22c20
giancarlosio <giancarlos.isasi@gmail.com>
2016-10-31 04:04:30 -0500

    Add sliders for the publications and guides section

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bxslider
 //= require turbolinks
 //= require angular
 //= require angular-rails-templates
---
+++ app/assets/javascripts/landing.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $('.slider1').bxSlider({
+    minSlides: 3,
+    maxSlides: 3,
+    slideWidth: 350,
+    slideMargin: 10
+  });
+  $('.slider2').bxSlider({
+    minSlides: 3,
+    maxSlides: 3,
+    slideWidth: 350,
+    slideMargin: 10
+  });
+});
