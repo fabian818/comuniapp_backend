816bf7c6dd056be9103ba106236461c83e17abd2
giancarlosio <giancarlos.isasi@gmail.com>
2016-10-31 12:26:28 -0500

    Delete extra spaces in home angular-controller

--- app/assets/javascripts/client/controllers/home.controller.js
+++ app/assets/javascripts/client/controllers/home.controller.js
@@ -8,17 +8,17 @@
 	function HomeController($scope, $state, $stateParams, $timeout) {
 		$timeout(function() {
 			jQuery('.slider1').bxSlider({
-		    minSlides: 3,
-		    maxSlides: 3,
-		    slideWidth: 350,
-		    slideMargin: 10
-		  });
-		  jQuery('.slider2').bxSlider({
-		    minSlides: 3,
-		    maxSlides: 3,
-		    slideWidth: 350,
-		    slideMargin: 10
-		  });
+				minSlides: 3,
+				maxSlides: 3,
+				slideWidth: 350,
+				slideMargin: 10
+			});
+			jQuery('.slider2').bxSlider({
+				minSlides: 3,
+				maxSlides: 3,
+				slideWidth: 350,
+				slideMargin: 10
+			});
 		}, 100);
 	}
 })();
