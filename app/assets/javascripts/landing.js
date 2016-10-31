a04e7ba1b26dccc9bbdab49234b68a3b6ce33c57
giancarlosio <giancarlos.isasi@gmail.com>
2016-10-31 12:23:36 -0500

    Remove turbolinks and refactor sliders scripts

--- app/assets/javascripts/landing.js
+++ app/assets/javascripts/landing.js
@@ -1,14 +1,2 @@
 $(document).ready(function(){
-  $('.slider1').bxSlider({
-    minSlides: 3,
-    maxSlides: 3,
-    slideWidth: 350,
-    slideMargin: 10
-  });
-  $('.slider2').bxSlider({
-    minSlides: 3,
-    maxSlides: 3,
-    slideWidth: 350,
-    slideMargin: 10
-  });
 });
